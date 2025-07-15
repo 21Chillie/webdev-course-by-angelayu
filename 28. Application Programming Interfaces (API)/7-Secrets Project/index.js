218e2bba4063e7646c66f758748821360ebe5451
Chillie <reskialamsyah@gmail.com>
2025-07-15 10:31:05 +0800

    Finish Section 28 – add related code and resources

---
+++ 28. Application Programming Interfaces (API)/7-Secrets Project/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const PORT = 3000;
+const API_URL = "https://secrets-api.appbrewery.com";
+
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+	try {
+		const result = await axios.get(`${API_URL}/random`);
+
+		res.render("index.ejs", {
+			secret: result.data.secret,
+			user: result.data.username,
+		});
+	} catch (err) {
+		res.status(404).send(err.response.data);
+	}
+});
+
+app.listen(PORT, () => {
+	console.log(`Server listening on http://localhost:${PORT}`);
+});
