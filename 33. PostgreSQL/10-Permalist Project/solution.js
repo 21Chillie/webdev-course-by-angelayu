76fa151e994b4f7f733d28d6d6790b34ae643079
Chillie <reskialamsyah@gmail.com>
2025-09-28 01:46:25 +0800

    UPDATE: setup postgres connection info

--- 33. PostgreSQL/10-Permalist Project/solution.js
+++ 33. PostgreSQL/10-Permalist Project/solution.js
@@ -1,16 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "permalist",
-  password: "123456",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
 });
 db.connect();
 
